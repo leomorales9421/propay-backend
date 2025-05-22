0b51d754ae70f50f513157471d3f0dd96ad5ba3b
Leo Morales <leo.morales.itoeste@gmail.com>
2025-05-22 00:07:56 -0400

    fix: ajustes de validacion y uploads que ignore el git

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,4 +1,5 @@
 import {
+    IsDefined,
     IsNotEmpty,
     IsString,
     MaxLength,
@@ -6,22 +7,25 @@ import {
 } from 'class-validator';
 
 export class CreateRestaurantDto {
-
-    @IsNotEmpty({ message: 'El nombre es obligatorio' })
+    @IsDefined({ message: 'El nombre es obligatorio' })
+    @IsNotEmpty({ message: 'El nombre no debe estar vacío' })
     @IsString({ message: 'El nombre debe ser texto' })
     @MaxLength(299, { message: 'El nombre no puede tener más de 299 caracteres' })
     name: string;
 
-    @IsNotEmpty({ message: 'La dirección es obligatoria' })
+    @IsDefined({ message: 'La dirección es obligatoria' })
+    @IsNotEmpty({ message: 'La dirección no debe estar vacía' })
     @IsString({ message: 'La dirección debe ser texto' })
     @MaxLength(299, { message: 'La dirección no puede tener más de 299 caracteres' })
     address: string;
 
-    @IsNotEmpty({ message: 'El número de teléfono es obligatorio' })
+    @IsDefined({ message: 'El número de teléfono es obligatorio' })
+    @IsNotEmpty({ message: 'El número de teléfono no debe estar vacío' })
     @IsPhoneNumber('VE', { message: 'El número de teléfono debe ser válido para Venezuela (ej: +58...)' })
     phone: string;
 
-    @IsNotEmpty({ message: 'El estado es obligatorio' })
+    @IsDefined({ message: 'El estado es obligatorio' })
+    @IsNotEmpty({ message: 'El estado no debe estar vacío' })
     @IsString({ message: 'El estado debe ser texto' })
     @MaxLength(299, { message: 'El estado no puede tener más de 299 caracteres' })
     state: string;
